8e35b19d7a58243afb96089a5257d62715cdbec4
Nick Barry <itsnickbarry@protonmail.ch>
2024-06-20 19:11:52 +0400

    apply prettier formatting

--- tasks/remove_logs.js
+++ tasks/remove_logs.js
@@ -10,39 +10,45 @@ const {
   TASK_COMPILE_SOLIDITY_GET_DEPENDENCY_GRAPH,
 } = require('hardhat/builtin-tasks/task-names');
 
-task('remove-logs', 'Removes console.log calls and imports from local source files', async function (args, hre) {
-  try {
-    await hre.run(TASK_COMPILE);
-  } catch (e) {
-    throw new HardhatPluginError('failed to compile contracts before removing logs');
-  }
+task(
+  'remove-logs',
+  'Removes console.log calls and imports from local source files',
+  async function (args, hre) {
+    try {
+      await hre.run(TASK_COMPILE);
+    } catch (e) {
+      throw new HardhatPluginError(
+        'failed to compile contracts before removing logs',
+      );
+    }
 
-  const sourcePaths = await hre.run(
-    TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS
-  );
+    const sourcePaths = await hre.run(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS);
 
-  const sourceNames = await hre.run(
-    TASK_COMPILE_SOLIDITY_GET_SOURCE_NAMES,
-    {
+    const sourceNames = await hre.run(TASK_COMPILE_SOLIDITY_GET_SOURCE_NAMES, {
       sourcePaths,
-    }
-  );
+    });
 
-  let graph = await hre.run(TASK_COMPILE_SOLIDITY_GET_DEPENDENCY_GRAPH, {
-    sourceNames,
-  });
+    let graph = await hre.run(TASK_COMPILE_SOLIDITY_GET_DEPENDENCY_GRAPH, {
+      sourceNames,
+    });
 
-  let count = 0;
+    let count = 0;
 
-  graph.getResolvedFiles().forEach(function ({ absolutePath, content }) {
-    const { rawContent } = content;
-    if (rawContent.includes('console.log') || rawContent.includes('console.sol')) {
-      let output = rawContent.replace(regexp.imports, '').replace(regexp.calls, '');
+    graph.getResolvedFiles().forEach(function ({ absolutePath, content }) {
+      const { rawContent } = content;
+      if (
+        rawContent.includes('console.log') ||
+        rawContent.includes('console.sol')
+      ) {
+        let output = rawContent
+          .replace(regexp.imports, '')
+          .replace(regexp.calls, '');
 
-      fs.writeFileSync(absolutePath, output);
-      count++;
-    }
-  });
+        fs.writeFileSync(absolutePath, output);
+        count++;
+      }
+    });
 
-  console.log(`Removed logs from ${ count } sources.`);
-});
+    console.log(`Removed logs from ${count} sources.`);
+  },
+);
